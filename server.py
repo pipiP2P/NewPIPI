27baf4f653d62030965013943342e6ac00c76457
pipiP2P <pipiP2P@users.noreply.github.com>
2015-03-12 17:55:00 +0200

    Update server.py

--- server.py
+++ server.py
@@ -10,12 +10,14 @@ udp_socket.bind(('0.0.0.0', UDP_PORT))
 debug_print("Succeed binding on port " + str(UDP_PORT) )
 while True:
     data, addr = udp_socket.recvfrom(4096)
-    print data
+    print 'DATA: ', data
     if data == "Hello":
         if ADDRESSES:
-            udp_socket.sendto(';'.join(ADDRESSES), (addr[0], UDP_PORT))
+            print 'Sending ADDRESSES To: ', addr
+            udp_socket.sendto(';'.join(ADDRESSES), addr)
         else:
-            udp_socket.sendto("None",(addr[0], UDP_PORT))
+            print 'Sending None To: ', addr
+            udp_socket.sendto("None", addr)
         if addr not in ADDRESSES:
             # Todo: Check how much people send to the client
-            ADDRESSES.append(addr)
+            ADDRESSES.append(addr[0])
