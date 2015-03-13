65ad1050cd3f60ec74027473ee99b9198c9145db
Ben <ben@barnea.com>
2015-03-13 18:20:04 +0200

    Connecting to server fixed

--- server.py
+++ server.py
@@ -3,11 +3,11 @@ import socket
 def debug_print(data):
     print data
 
-UDP_PORT = 50000
+UDP_PORT = 60000
 ADDRESSES = []
 udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 udp_socket.bind(('0.0.0.0', UDP_PORT))
-debug_print("Succeed binding on port " + str(UDP_PORT) )
+debug_print("Succeed binding on port " + str(UDP_PORT))
 while True:
     data, addr = udp_socket.recvfrom(4096)
     print 'DATA: ', data
