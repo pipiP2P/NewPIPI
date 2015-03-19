d389af4b57cf6e0f8a6c91ac26c70193a54407da
pipiP2P <pipiP2P@users.noreply.github.com>
2015-03-19 18:21:53 +0200

    Update client.py

--- client.py
+++ client.py
@@ -6,15 +6,20 @@ import File
 from Functions import *
 from Protocol import *
 
-SERVER_IP = "192.168.0.109"
+SERVER_IP = "10.10.10.226"
 addr = SERVER_IP
 ADDRESSES = []
 OUR_FILES = []
 PEERS_FILES = []
 UDP_PORT = 50000
+DOWNLOAD_PORT = 50001
 BUFFER_SIZE = 4096
 TIMEOUT = 2
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+def send_msg_to_all(msg, sending_socket):
+    for addr in ADDRESSES:
+        sending_socket.send(msg, (addr, UDP_PORT))
 
 
 def initialize():
@@ -74,6 +79,8 @@ def new_connection(addr):
 
 
 def send_files_list(addr):
+    check_our = True
+    check_peers = True
     debug_print("{0} asked us for our files list!".format(addr))
     if addr not in ADDRESSES:
         ADDRESSES.append(addr)
@@ -83,9 +90,16 @@ def send_files_list(addr):
         for file_object in OUR_FILES:
             content += file_object.to_string() + ';'
         content = content[:-1]  # Remove the last ;
-    else:
+        check_our = False
+    if PEERS_FILES:
+        for file_object in PEERS_FILES:
+            content += file_object.to_string() + ';'
+        content = content[:-1]
+        check_peers = False
+    elif check_peers and check_our:
         content += "None"
     client_socket.sendto(content, (addr, UDP_PORT))
+
 
 
 def get_files_list(data):
@@ -111,19 +125,40 @@ def UDP_listen():
     """
     new_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     new_socket.bind(("0.0.0.0", UDP_PORT))
+    content = ''
     while True:
-        data, addr = new_socket.recvfrom(BUFFER_SIZE)
-        addr = addr[0]
-        debug_print("Received this data " + data + " from " + addr)
+        rlist, wlist, xlist = select.select([new_socket], [new_socket], [])
+        for read_socket in rlist:
+            data, addr = new_socket.recvfrom(BUFFER_SIZE)
+            addr = addr[0]
+            debug_print("Received this data " + data + " from " + addr)
 
-        if data == "Hello":
-            new_connection(addr)
+            if data == "Hello":
+                new_connection(addr)
 
-        elif data == "show-files":
-            send_files_list(addr)
+            elif data == "show-files":
+                send_files_list(addr)
 
-        elif data.startswith("FILES:"):
-            get_files_list(data)
+            elif data.startswith("FILES:"):
+                get_files_list(data)
+
+            elif data.startswith("PARTCHECK;"):
+
+
+        if msvcrt.kbhit():
+            key = msvcrt.getch()
+            # if user hit enter key
+            if key == chr(13):
+                sys.stdout.write('\n')
+                msg = request_part(content)
+                send_msg_to_all(msg, new_socket)
+                content = ''
+            elif key == chr(127) or key == chr(8):
+                content = content[:-1]
+                sys.stdout.write("\b")
+            else:
+                content += key
+                sys.stdout.write(key)
 
 
 def print_addresses():
@@ -185,6 +220,64 @@ def connect_to_server():
         else:
             addr = raw_input("Could not connect to the IP " + addr + "\nPlease write another IP\n")
     new_listen_socket.close()
+
+
+def receive_name():
+    content = ''
+    future_send = False
+    while not future_send:
+        # if user hit a key
+        if msvcrt.kbhit():
+            key = msvcrt.getch()
+            content += key
+            # if user hit enter key
+            if key == chr(13):
+                future_send = True
+                sys.stdout.write('\n')
+            if key == chr(127) or key == chr(8):
+                content = content[:-1]
+                sys.stdout.write("\b")
+            else:
+                sys.stdout.write(key)
+    return content
+
+
+def downloader():
+    downloader_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    downloader_socket.bind(('0.0.0.0', DOWNLOAD_PORT))
+
+
+def request_part(name):
+    file_requested = find_file_list_name(name, OUR_FILES)
+    final_msg = None
+    if file_requested:
+        final_msg = "PARTCHECK;" + name + ";" + file_requested.get_hash()
+
+    return final_msg
+
+
+
+def find_file_list_name(name, list_search):
+    for my_file in list_search:
+        if my_file.get_name() == name:
+            return my_file
+    return None
+
+
+def find_file_list_hash(hash, list_search):
+    for my_file in list_search:
+        if my_file.get_hash() == hash:
+            return my_file
+    return None
+
+
+def handle_part_request(name, hash):
+    requested_file = find_file_list(hash, OUR_FILES)
+    if requested_file:
+        # Send which parts we have
+    else:
+        # Send we have no parts
+
 
 
 if __name__ == '__main__':
